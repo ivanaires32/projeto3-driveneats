b3f1fab3b1147a99a462f3dc8b84c948a7f2f0f6
Ivan <ivanaires32@gmail.com>
2023-01-03 10:44:26 -0300

    icones de confirmação

--- js/script.js
+++ js/script.js
@@ -1,65 +1,111 @@
+
+
 function selecionarFrango(){
-    document.getElementById('frango').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('carne').style.boxShadow = "none";
-    document.getElementById('peixe').style.boxShadow = "none";
+    frango.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    carne.style.boxShadow = "none";
+    peixe.style.boxShadow = "none";
+
+    iconeFrango.style.display = "block";
+    iconeCarne.style.display = "none";
+    iconePeixe.style.display = "none";
 }
 
 function selecionarCarne(){
-    document.getElementById('carne').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('frango').style.boxShadow = "none";
-    document.getElementById('peixe').style.boxShadow = "none";
+    carne.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    frango.style.boxShadow = "none";
+    peixe.style.boxShadow = "none";
+
+    iconeCarne.style.display = "block";
+    iconeFrango.style.display = "none";
+    iconePeixe.style.display = "none";
 }
 
 function selecionarPeixe(){
-    document.getElementById('peixe').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('carne').style.boxShadow = "none";
-    document.getElementById('frango').style.boxShadow = "none";
+    peixe.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    carne.style.boxShadow = "none";
+    frango.style.boxShadow = "none";
+
+    iconePeixe.style.display = "block";
+    iconeFrango.style.display = "none";
+    iconeCarne.style.display = "none";
 }
 
 //******************************** */
 
+
+
+
 function selecionarCoca(){
-    document.getElementById('coca').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('guarana').style.boxShadow = "none";
-    document.getElementById('suco').style.boxShadow = "none";
+    coca.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    guarana.style.boxShadow = "none";
+    suco.style.boxShadow = "none";
+
+    iconeCoca.style.display = "block";
+    iconeGuarana.style.display = "none";
+    iconeSuco.style.display = "none";
 }
 
 function selecionarGuarana(){
-    document.getElementById('guarana').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('coca').style.boxShadow = "none";
-    document.getElementById('suco').style.boxShadow = "none";
+    guarana.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    coca.style.boxShadow = "none";
+    suco.style.boxShadow = "none";
+
+    iconeGuarana.style.display = "block";
+    iconeCoca.style.display = "none";
+    iconeSuco.style.display = "none";
 }
 
 function selecionarSuco(){
-    document.getElementById('suco').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('guarana').style.boxShadow = "none";
-    document.getElementById('coca').style.boxShadow = "none";
+    suco.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    guarana.style.boxShadow = "none";
+    coca.style.boxShadow = "none";
+
+    iconeSuco.style.display = "block";
+    iconeGuarana.style.display = "none";
+    iconeCoca.style.display = "none";
 }
 
 /*************************** */
 
+
+
 function selecionarPudim(){
-    document.getElementById('pudim').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('bolo').style.boxShadow = "none";
-    document.getElementById('sorvete').style.boxShadow = "none";
+    pudim.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    bolo.style.boxShadow = "none";
+    sorvete.style.boxShadow = "none";
+
+    iconePudim.style.display = "block";
+    iconeBolo.style.display = "none";
+    iconeSorvete.style.display = "none";
 }
 
 function selecionarBolo(){
-    document.getElementById('bolo').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('pudim').style.boxShadow = "none";
-    document.getElementById('sorvete').style.boxShadow = "none";
+    bolo.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    pudim.style.boxShadow = "none";
+    sorvete.style.boxShadow = "none";
+
+    iconeBolo.style.display = "block";
+    iconePudim.style.display = "none";
+    iconeSorvete.style.display = "none";
 }
 
 function selecionarSorvete(){
-    document.getElementById('sorvete').style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
-    document.getElementById('bolo').style.boxShadow = "none";
-    document.getElementById('pudim').style.boxShadow = "none";
+    sorvete.style.boxShadow = "inset 0px 0px 0px 5px #32B72F";
+    bolo.style.boxShadow = "none";
+    pudim.style.boxShadow = "none";
+
+    iconeSorvete.style.display = "block";
+    iconeBolo.style.display = "none";
+    iconePudim.style.display = "none";
 }
 
 /******************** */
 
-function fecharPedido(){
-    if(selecionarBolo || selecionarSorvete || selecionarPudim == true){
-        document.getElementById('fecharPedido').style.display = "block";
-    }
-}
+/* const pudim =  document.getElementById('pudim');
+const bolo = document.getElementById('bolo');
+const sorvete = document.getElementById('sorvete');
+
+const iconePudim = document.getElementById('iconePudim');
+const iconeBolo = document.getElementById('iconeBolo');
+const iconeSorvete = document.getElementById('iconeSorvete');
+*/
