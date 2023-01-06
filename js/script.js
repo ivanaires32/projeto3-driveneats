2e39ecb7c177871a12e574cde0ed0fd5e39fb537
Ivan Aires <ivanaires32@gmail.com>
2023-01-05 22:35:52 -0300

    Alterado com propriedade This

--- js/script.js
+++ js/script.js
@@ -4,108 +4,66 @@ let pedidoSobremesa;
 
 function selecionarPrato(pratoSelecionado) {
 
-    const outraOpcao = document.querySelector('.selecionado');
+    const outraOpcao = document.querySelector('.prato .selecionado');
     if (outraOpcao !== null) {
         outraOpcao.classList.remove('selecionado');
     }
-    const opcaoSelecionada = document.querySelector(pratoSelecionado);
-    opcaoSelecionada.classList.add('selecionado');
+    pratoSelecionado.classList.add('selecionado');
 
-    if (pratoSelecionado === '.frango') {
-        document.querySelector(".iconeFrango").classList.add('block');
-        pedidoPrato = ["Frango Yin Yang", 14.90]
-    } else {
-        document.querySelector(".iconeFrango").classList.remove('block');
+    const semCheck = document.querySelector('.prato .block');
+    if (semCheck !== null) {
+        semCheck.classList.remove('block')
     }
-    if (pratoSelecionado === '.carne') {
-        document.querySelector(".iconeCarne").classList.add('block');
-        pedidoPrato = ["Churrasco", 17.90];
-    } else {
-        document.querySelector(".iconeCarne").classList.remove('block');
-    } if (pratoSelecionado === '.peixe') {
-        document.querySelector(".iconePeixe").classList.add('block');
-        pedidoPrato = ["Peixe Frito", 12.90]
-    } else {
-        document.querySelector(".iconePeixe").classList.remove('block');
-    }
+    const check = document.querySelector('.prato .selecionado .icone');
+    check.classList.add('block');
+
+    pedidoPrato = true;
+
 }
 
 
 function selecionarBebida(bebidaSelecionada) {
 
-    const outraOpcao = document.querySelector('.selecionadoBebida');
+    const outraOpcao = document.querySelector('.bebida .selecionado');
     if (outraOpcao !== null) {
-        outraOpcao.classList.remove('selecionadoBebida');
+        outraOpcao.classList.remove('selecionado');
     }
-    const opcaoSelecionada = document.querySelector(bebidaSelecionada);
-    opcaoSelecionada.classList.add('selecionadoBebida');
+    bebidaSelecionada.classList.add('selecionado');
 
-    if (bebidaSelecionada === '.coca') {
-        document.querySelector(".iconeCoca").classList.add('block');
-        pedidoBebida = ["Coca-Cola", 4.90]
-    } else {
-        document.querySelector(".iconeCoca").classList.remove('block');
+    const semCheck = document.querySelector('.bebida .block');
+    if (semCheck !== null) {
+        semCheck.classList.remove('block')
     }
-    if (bebidaSelecionada === '.guarana') {
-        document.querySelector(".iconeGuarana").classList.add('block');
-        pedidoBebida = ["Guaraná", 3.90];
-    } else {
-        document.querySelector(".iconeGuarana").classList.remove('block');
-    } if (bebidaSelecionada === '.suco') {
-        document.querySelector(".iconeSuco").classList.add('block');
-        pedidoBebida = ["Suco", 6.90]
-    } else {
-        document.querySelector(".iconeSuco").classList.remove('block');
-    }
+    const check = document.querySelector('.bebida .selecionado .icone');
+    check.classList.add('block');
+
+    pedidoBebida = true;
+
 }
 
 //******************************** */
 
 function selecionarSobremesa(sobremesaSelecionada) {
-    const outraOpcao = document.querySelector('.selecionadoSobremesa');
+    const outraOpcao = document.querySelector('.sobremesa .selecionado');
     if (outraOpcao !== null) {
-        outraOpcao.classList.remove('selecionadoSobremesa');
+        outraOpcao.classList.remove('selecionado');
     }
+    sobremesaSelecionada.classList.add('selecionado');
 
-    const opcaoSelecionada = document.querySelector(sobremesaSelecionada);
-    opcaoSelecionada.classList.add('selecionadoSobremesa');
-
-    if (sobremesaSelecionada === '.pudim') {
-        document.querySelector(".iconePudim").classList.add('block');
-        pedidoSobremesa = ["Pudim", 7.90];
-    } else {
-        document.querySelector(".iconePudim").classList.remove('block');
+    const semCheck = document.querySelector('.sobremesa .block');
+    if (semCheck !== null) {
+        semCheck.classList.remove('block');
     }
+    const check = document.querySelector('.sobremesa .selecionado .icone');
+    check.classList.add('block');
 
-    if (sobremesaSelecionada === '.bolo') {
-        document.querySelector('.iconeBolo').classList.add('block');
-        pedidoSobremesa = ["Bolo de Chocolate", 8.90]
-    } else {
-        document.querySelector('.iconeBolo').classList.remove('block');
-    }
-
-    if (sobremesaSelecionada === '.sorvete') {
-        document.querySelector('.iconeSorvete').classList.add('block');
-        pedidoSobremesa = ["Sorvete de Baunilha", 6.90]
-    } else {
-        document.querySelector('.iconeSorvete').classList.remove('block');
-    }
+    pedidoSobremesa = true;
 }
 
 /******************** */
 
 function pedidoFinalizado() {
-    if (
-        (pedidoPrato[0] === 'Frango Yin Yang' ||
-            pedidoPrato[0] === 'Churrasco' ||
-            pedidoPrato[0] === 'Peixe Frito') &&
-        (pedidoBebida[0] === 'Coca-Cola' ||
-            pedidoBebida[0] === 'Guaraná' ||
-            pedidoBebida[0] === 'Suco') &&
-        (pedidoSobremesa[0] === 'Pudim' ||
-            pedidoSobremesa[0] === 'Bolo de Chocolate' ||
-            pedidoSobremesa[0] === 'Sorvete de Baunilha')
-    ) {
+    if (pedidoPrato === true && pedidoBebida === true && pedidoSobremesa === true) {
         const botaoFinalizar = document.querySelector('button');
         botaoFinalizar.classList.add('fazerPedido')
         botaoFinalizar.innerHTML = 'Fechar Pedido';
