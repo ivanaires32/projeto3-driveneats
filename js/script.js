99c68b01c61be75276939fa3ec9e7df1d30d3fcd
Ivan Aires <105604008+ivanaires3@users.noreply.github.com>
2023-01-07 20:27:48 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -107,10 +107,8 @@ function fecharOPedido() {
     const ValorPrato = Number(precoPrato);
     const ValorBebida = Number(precoBebida);
     const ValorSobremesa = Number(precoSobremesa);
-    const somaValor = ValorBebida + ValorPrato + ValorSobremesa;
-    const corrigirValor = (somaValor * 10) / 10;
     const texto = `Olá, gostaria de fazer o pedido:\n- Prato: ${pedidoPrato}\n- Bebida: ${pedidoBebida}\n- Sobremesa: ${pedidoSobremesa}\n
-    Total: R$ ${corrigirValor}`;
+    Total: R$ ${(ValorPrato * 10 + ValorBebida * 10 + ValorSobremesa * 10) / 10}`;
 
     const textoCodificado = encodeURIComponent(texto);
     window.open(`https://wa.me/+5591999999999?text=${textoCodificado}`);
